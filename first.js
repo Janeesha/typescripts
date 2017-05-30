85b51344ee06b89a01360e41e255046610fa7e26
unknown <hima.ratikanti@gmail.com>
2017-05-30 14:48:13 +0530

    basic

---
+++ first.js
@@ -0,0 +1,13 @@
+var employeeId;
+var firstname = "asha";
+var istrue = true;
+var userobject = null;
+var studentdetails;
+studentdetails = 2;
+studentdetails = "heelo";
+var studentdetail;
+studentdetail = [2];
+studentdetail = ["heelo"];
+var sample = function () {
+    return "hello";
+};
---
+++ first.ts
@@ -0,0 +1,17 @@
+var employeeId:number;
+
+var firstname:string="asha";
+var istrue:boolean=true;
+
+var userobject:object=null;
+var studentdetails:any;
+studentdetails=2;
+studentdetails="heelo";
+
+var studentdetail:any[];
+studentdetail=[2];
+studentdetail=["heelo"];
+
+var sample=function(){
+	return "hello"
+}
